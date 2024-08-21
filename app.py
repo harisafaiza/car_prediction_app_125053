4100ee6853cefb719451ed08b88d58ba1d832c60
HARISA FAIZA D <127124299+harisafaiza@users.noreply.github.com>
2024-08-21 17:51:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-import pandas as pd
-import numpy as np
-import pickle as pk
 import streamlit as st
+from PIL import Image
 
-# Load the pre-trained model
-model = pk.load(open('model.pkl', 'rb'))
-
-# Header of the Web App
-st.header('Car Price Prediction ML Model')
+# Set the page config
+st.set_page_config(
+    page_title="Car Price Prediction",
+    page_icon=":car:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
 
 # Load the car details CSV
 cars_data = pd.read_csv('Cars.csv')
@@ -19,18 +24,45 @@ def get_brand_name(car_name):
 # Apply the function to extract brand names from the 'name' column
 cars_data['name'] = cars_data['name'].apply(get_brand_name)
 
+# Load the pre-trained model
+model = pk.load(open('model.pkl', 'rb'))
+
+# Set the background color and font color
+st.set_page_config(
+    page_title="Car Price Prediction",
+    page_icon=":car:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    },
+    page_icon='🚗',
+    page_background_color='#ff8c00',  # Orange
+    page_text_color='#ffffff',  # White
+)
+
+# Header of the Web App
+st.title('Car Price Prediction ML Model', anchor=None)
+
 # Input fields for the user to interact with
-name = st.selectbox('Select Car Brand', cars_data['name'].unique())
-year = st.slider('Car Manufactured Year', 1994, 2024)
-km_driven = st.slider('No of kms Driven', 11, 200000)
-fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
-seller_type = st.selectbox('Seller type', cars_data['seller_type'].unique())
-transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
-owner = st.selectbox('Owner type', cars_data['owner'].unique())
-mileage = st.slider('Car Mileage (km/l)', 10, 40)
-engine = st.slider('Engine Capacity (CC)', 700, 5000)
-max_power = st.slider('Max Power (bhp)', 0, 200)
-seats = st.slider('Number of Seats', 2, 10)
+col1, col2 = st.columns(2)
+
+with col1:
+    name = st.selectbox('Select Car Brand', cars_data['name'].unique())
+    fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
+    transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
+    owner = st.selectbox('Owner type', cars_data['owner'].unique())
+
+with col2:
+    year = st.number_input('Car Manufactured Year', min_value=1994, max_value=2024, step=1)
+    km_driven = st.number_input('No of kms Driven', min_value=11, max_value=200000, step=1)
+    seller_type = st.selectbox('Seller type', cars_data['seller_type'].unique())
+    mileage = st.number_input('Car Mileage (km/l)', min_value=10, max_value=40, step=1)
+    engine = st.number_input('Engine Capacity (CC)', min_value=700, max_value=5000, step=1)
+    max_power = st.number_input('Max Power (bhp)', min_value=0, max_value=200, step=1)
+    seats = st.number_input('Number of Seats', min_value=2, max_value=10, step=1)
 
 # Predict button
 if st.button("Predict"):
@@ -52,7 +84,6 @@ if st.button("Predict"):
                                       'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
                                       'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
                                      range(1, 32), inplace=True)
-
     try:
         # Perform prediction
         car_price = model.predict(input_data_model)
